a3cc7dde2e4a49fb0369f54a18174dab69aee044
Ethan <G00467054@atu.ie>
2025-10-21 20:53:37 +0100

    Part 1

---
+++ src/main/java/ie/atu/week6/TryMain.java
@@ -0,0 +1,22 @@
+package ie.atu.week6;
+
+import java.util.Scanner;
+
+public class TryMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = 0;
+        while(true){
+            System.out.print("Enter your age: ");
+            String text = sc.nextLine().trim();
+            try {
+                age = Integer.parseInt(text);
+                System.out.println("Thanks! You Entered age = " + age);
+                break;
+            }
+            catch (NumberFormatException e){
+                System.out.println("Invalid input" + e.getMessage());
+            }
+        }
+    }
+}
